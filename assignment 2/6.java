3e5430e83eb979fb328ed79ee832880aac7188d4
aabhaschopra <aabhaschopra@gmail.com>
2022-09-15 21:15:21 +0530

    Update 6.java

---
+++ assignment 2/6.java
@@ -0,0 +1,10 @@
+class MultiplesOfThree {
+    public static void main(String args[]) {
+        int multiples = 3;
+
+        while (multiples <= 36) {
+            System.out.print(multiples + " ");
+            multiples = multiples + 3;
+        }
+    }
+}
