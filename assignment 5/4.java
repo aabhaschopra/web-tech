8af769389f9d04b851a926e39c9de2e43e480b2f
aabhaschopra <aabhaschopra@gmail.com>
2022-11-22 16:57:40 +0530

    Update

---
+++ assignment 5/4.java
@@ -0,0 +1,23 @@
+// There are two mistakes in the following snippet, one is the colon
+// instead of the semicolon in the m1 function and the second is that the
+// v in Void is capital, once we correct the following changes, we get 24 as output.
+
+public class Myclass {
+  static int a = 20;
+
+  Myclass() {
+    a++;
+  }
+
+  Void m1() {
+    a++;
+    System.out.println(a):
+  }
+
+  public static void main(String[] args) {
+    Myclass obj = new Myclass();
+    Myclass obj2 = new Myclass();
+    Myclass obj3 = new Myclass();
+    obj3.m1();
+  }
+}
---
+++ assignment 5/5.java
@@ -0,0 +1,13 @@
+// The output of this code snippet will be 10.
+
+class Myclass {
+  final static int x;
+
+  static {
+    x = 10;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(x);
+  }
+}
