cef99e20350c8251be5a062399fbdd09ea161d74
aabhaschopra <aabhaschopra@gmail.com>
2022-11-22 16:51:37 +0530

    Create 2.java

--- assignment 5/1.java
+++ assignment 5/1.java
@@ -1,1 +1,38 @@
+class Animals {
+  public static void main(String args[]) {
+    Mammals mammalsObject = new Mammals();
+    MarineAnimals marineAnimalsObject = new MarineAnimals();
+    BlueWhale blueWhaleObject = new BlueWhale();
+    
+    mammalsObject.print(); // 1
+    marineAnimalsObject.print(); // 2
+    blueWhaleObject.print(); // 3
 
+    // Since multiple inheritance is not supported, the 4th point cannot be
+    // implemented,
+    // thus the first three are done only without any inheritance.
+  }
+}
+
+/*
+ * Since multiple inheritance is not allowed in java, BlueWhale cannot inherit
+ * from both Mammals and MarineAnimals
+ */
+
+class Mammals {
+  void print() {
+    System.out.println("I am mammal");
+  }
+}
+
+class MarineAnimals {
+  void print() {
+    System.out.println("I am a marine animal");
+  }
+}
+
+class BlueWhale {
+  void print() {
+    System.out.println("I belong to both the categories: Mammals as well as Marine Animals");
+  }
+}
